d7a87fadd0ba8ad1ab466d37ea0809e4485a246d
Siyona <113326339+siyoyoCode@users.noreply.github.com>
2023-01-21 17:26:07 -0800

    idk if this will work i give up

--- Shwetasapplication9/Views/IPhone11ProXOneTwoView/IPhone11ProXOneTwoViewModel.swift
+++ Shwetasapplication9/Views/IPhone11ProXOneTwoView/IPhone11ProXOneTwoViewModel.swift
@@ -4,3 +4,9 @@ import SwiftUI
 class IPhone11ProXOneTwoViewModel: ObservableObject {
     @Published var nextScreen: String? = nil
 }
+
+struct IPhone11ProXOneTwoViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
